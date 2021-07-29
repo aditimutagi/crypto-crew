191a604d30354d69ee33e8929087db53ddc01b04
LisaJeong <LisaJeong@users.noreply.github.com>
2021-07-29 09:56:46 -0400

    ui update

--- src/Components/CryptoTable.js
+++ src/Components/CryptoTable.js
@@ -1,22 +1,22 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import './CryptoTable.css';
 
 class CryptoTable extends Component {
    constructor(props) {
       super(props)
       this.state = { //placeholder
          transactions: [
-            { id: 1, coin: "BTC/DOGE",   seller: "x5252906", buyer: "x2014509"},
-            { id: 2, coin: "ETHER/IOTA", seller: "x2586209", buyer: "x2586082"},
-            { id: 3, coin: "IOTA/BTC",   seller: "x5252096", buyer: "x0081901"}
+            { coin: "BTC/DOGE",   seller: "x525290690200252", buyer: "x20145091591156080181"},
+            { coin: "ETHER/IOTA", seller: "x258620962096051", buyer: "x2586082625902093051"},
+            { coin: "IOTA/BTC",   seller: "x525290690200252", buyer: "x008190159050149"}
          ]
       }
    }
    renderTableData() {
     return this.state.transactions.map((transaction, index) => {
-       const { id, coin, seller, buyer } = transaction
+       const { coin, seller, buyer } = transaction
        return (
-          <tr key={id}>
-             <td>{id}</td>
+          <tr key={coin}>
              <td>{coin}</td>
              <td>{seller}</td>
              <td>{buyer}</td>
